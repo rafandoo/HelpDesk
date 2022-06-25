cced0523fcf0d537c43ec1b7afbeed37840bc9bf
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-06-25 19:26:33 -0300

    upload configuração conexão banco

---
+++ src/config/conf.inc.php
@@ -0,0 +1,14 @@
+<?php
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', 'helpdesk');    
+    define('USER', 'root');  
+    define('PASSWORD', '');
+
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','HelpDesk');
+    define('DESCRICAO_DO_PROJETO','Um sistema de HelpDesk.');
+?>
