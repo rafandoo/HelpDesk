43b67b71506500607028d58f06d5d92b5db921c2
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-07-05 10:10:16 -0300

    Update autoload.php

--- src/util/autoload.php
+++ src/util/autoload.php
@@ -1,6 +1,18 @@
 <?php
+    /* A function that is called when a class is called. It checks if the class exists and if it does it
+    requires it. */
     spl_autoload_register(function ($nomeClasse) {
-    if (file_exists("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php"))
-        require_once("classes".DIRECTORY_SEPARATOR.$nomeClasse.".class.php");
+        if (file_exists("..\\class\\".$nomeClasse.".class.php")) {
+            require_once("..\\class\\".$nomeClasse.".class.php");
+            echo "Require: ".$nomeClasse."<br>";
+        } else if (file_exists("..\\src\\class\\".$nomeClasse.".class.php")) {
+            require_once("..\\src\\class\\".$nomeClasse.".class.php");
+            echo "Require: ".$nomeClasse."<br>";
+        } else if ("action\\".$nomeClasse.".php") {
+            require_once("action\\".$nomeClasse.".php");
+            echo "Require: ".$nomeClasse."<br>";
+        } else {
+            echo "Classe não encontrada";
+        }
     });
 ?>
